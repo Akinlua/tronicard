14c96921fa41152a5da12c19e6442c6158607157
Olorunfunminiyi <akinluaolorunfunminiyi@gmail.com>
2022-08-06 15:42:48 +0100

    fits

--- tronicardBlog/settings.py
+++ tronicardBlog/settings.py
@@ -128,15 +128,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = 'static/'
-MEDIA_URL='/images/'
+STATIC_URL = '/static/'
+MEDIA_URL='/media/'
 
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
